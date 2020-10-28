8ac3fd4dceecfe129da45287da9f3cedec80bf39
even-ewgen <ewgenia1997@gmail.com>
2020-10-28 21:21:58 +0500

    Версия со стартовой страницей - реализованы переходы на страницу с событием и переходы между ними.

--- src/main/java/text/quest/web/presentation/contrillers/TextQuestController.java
+++ src/main/java/text/quest/web/presentation/contrillers/TextQuestController.java
@@ -1,13 +1,14 @@
 package text.quest.web.presentation.contrillers;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import text.quest.web.domain.EventDto;
 import text.quest.web.domain.EventServices;
 
 @RestController //@Controller
-@RequestMapping("/quest")
 public class TextQuestController {
 
     private final EventServices eventServices;
@@ -16,7 +17,27 @@ public class TextQuestController {
         this.eventServices = eventServices;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/start", method = RequestMethod.GET)
+    public Model start(Model model) {
+        return model;
+    }
+
+    @RequestMapping(value = "/quest", method = RequestMethod.GET)
+    public Model getEventById(@RequestParam Integer id, Model model) {
+        EventDto eventDto = eventServices.getEvent(id);
+        model.addAttribute("text", eventDto.getEvent().getText());
+        model.addAttribute("optionText1", eventDto.getFirstOptions().getText());
+        model.addAttribute("nextEventId", eventDto.getFirstOptions().getNextEventId());
+
+        return model;
+    }
+
+     @RequestMapping(value = "/quest", method = RequestMethod.POST)
+    public @ResponseBody Integer getNextEventId(@RequestParam Integer id) {
+        return id;
+    }
+
+    /*    @RequestMapping(value = "/quest", method = RequestMethod.GET)
     public Model getEvent(Model model) {
         //EventDto eventDto = eventServices.getEvent(8);
 
@@ -24,14 +45,6 @@ public class TextQuestController {
         model.addAttribute("optionText1", "World");
         model.addAttribute("nextEventId", 8);
         return model;
-    }
+    }*/
 
-    @RequestMapping(value = "/quest", method = RequestMethod.GET)
-    public Model getEventForId(@RequestParam Integer id, Model model) {
-        System.out.println(id);
-        model.addAttribute("text", "World1");
-        model.addAttribute("optionText1", "World1");
-        model.addAttribute("nextEventId", 10);
-        return model;
-    }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,13 +2,10 @@ jQuery(document).ready(function() {
     var addr = window.location.origin;
 
     $('.button').click(function() {
-            var id = {"id":this.id};
-            $.ajax({
-                type: "GET",
-                dataType: 'json',
-                url: "/quest",
-                data: id
-            });
-        });
-
+        var id = {"id":this.id};
+        $.post("/quest", id, onAjaxSuccess);
+        function onAjaxSuccess(data) {// Здесь мы получаем данные, отправленные сервером и выводим их на экран.
+            window.location.href = '../quest?id=' + data;
+        }
+    });
 });
